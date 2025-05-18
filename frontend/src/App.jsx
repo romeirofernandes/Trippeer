8fb6b93b4e904fdab602824a02e586621f4d661f
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2025-05-18 12:30:33 +0530

    Pushy pushy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
+import Itinerary from "./components/Itinerary";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/itenirary" element={<Itinerary />} />
       </Routes>
     </BrowserRouter>
   );
